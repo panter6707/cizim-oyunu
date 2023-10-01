e4ddc8da83d9b783efd4c13597164404e1e73c3e
Kemal Yılmaz <kemalmky@gmail.com>
2023-10-02 00:36:50 +0300

    çizim oyunu v1.0

---
+++ main.py
@@ -0,0 +1,43 @@
+import turtle
+
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("light blue")
+drawing_board.title("Python Turtle")
+
+
+turtle_instance = turtle.Turtle()
+turtle_instance.color("red")
+
+def turtle_forward():
+    turtle_instance.forward(100)
+
+def turtle_left():
+    turtle_instance.left(90)
+
+def turtle_right():
+    turtle_instance.right(45)
+
+def clear_screen():
+    turtle_instance.clear()
+
+def turtle_return_home():
+    turtle_instance.home()
+
+def turtle_pen_up():
+    turtle_instance.penup()
+
+def turtle_pen_down():
+    turtle_instance.pendown()
+
+
+drawing_board.listen()
+drawing_board.onkey(fun=turtle_forward, key="a")
+drawing_board.onkey(fun=turtle_left,key= "z")
+drawing_board.onkey(fun=turtle_right,key= "s")
+drawing_board.onkey(fun=clear_screen,key= "x")
+drawing_board.onkey(fun=turtle_return_home,key= "c")
+drawing_board.onkey(fun=turtle_pen_up,key= "d")
+drawing_board.onkey(fun=turtle_pen_down,key= "f")
+
+turtle.mainloop()
